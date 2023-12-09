7f192030ab3250c553f9ff9ea069b5b123052910
HugoBDev <hugobrd.pro@gmail.com>
2023-12-09 13:47:06 +0100

    add markers & projets

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+import Project from "./project.model.js";
+
+
+
+// le point initial sur Henriville,
+// avec  sa Latitude :49.884195 et sa longitude :  2.299391
+const henrivilleLocation = [49.884195, 2.299391]
+
+
+// cration de la map (vide) 
+// set view prend deux parametres : lat long et zoom.
+var map = L.map('map').setView(henrivilleLocation, 16);
 
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,4 +18,16 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
-console.log('coucou');
+// les projets de tata Camille ! 
+const camHome = new Project(49.8817568673084, 2.2977756999391756,'5 Rue Camille Saint-Saëns, 80000 Amiens', true);
+const home1 = new Project(49.88244987606139, 2.295839761994673,'324 Rue mon cul, 80000 Amiens', true);
+
+
+// la liste des projets
+const projectsMarkers = [camHome, home1]
+
+
+// chaque marker de "Markers" (le tableau) est representé par "m"
+projectsMarkers.forEach(pm => {
+    var marker = L.marker(pm.coordinate).addTo(map).bindPopup(pm.address);
+})
---
+++ project.model.js
@@ -0,0 +1,16 @@
+
+
+export default class Project{
+
+    constructor(lat, long, address, display){
+        this.lat = lat,
+        this.long = long,
+        this.coordinate = [this.lat, this.long],
+        this.address = address,
+        this.display = display
+    }
+
+
+
+
+}
