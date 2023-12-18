328d117832fed21e8f9d9a6295c6e20373a67a9f
HugoBDev <hugobrd.pro@gail.com>
2023-12-18 17:46:43 +0100

    close button event listener

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const projectApi = new ProjectApi
 // avec  sa Latitude :49.884195 et sa longitude :  2.299391
 const henrivilleLocation = [49.884195, 2.299391];
 
+const closeButton = document.getElementsByClassName('close-button')[0]; 
+//TODO close the pop up // 
+closeButton.addEventListener('click', (e) =>{
+  console.log(e);
+})
+
+
 // cration de la map (vide)
 // set view prend deux parametres : lat long et zoom.
 var map = L.map("map").setView(henrivilleLocation, 16);
@@ -16,19 +23,7 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-// les projets de tata Camille !
-const camHome = new Project(
-  49.8817568673084,
-  2.2977756999391756,
-  "5 Rue Camille Saint-Saëns, 80000 Amiens",
-  true
-);
-const home1 = new Project(
-  49.88244987606139,
-  2.295839761994673,
-  "324 Rue mon cul, 80000 Amiens",
-  true
-);
+
 
 projectApi.getProjects()
     .then(projects => {
